9bb376375de0b939f8e4afb046ec8a301238ce51
Salman Khimani <sk.salman.khimani@gmail.com>
2018-07-25 15:51:03 +0500

    configuration changes

--- backend/config.php
+++ backend/config.php
@@ -3,11 +3,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
 // define constants
-if (preg_match('/localhost/', $_SERVER['HTTP_HOST'])) {
-    define('APP_URL', 'http://theappsgalore.localhost/sv/');
-} else {
-    define('APP_URL', 'http://theappsgalore.com/sv/');
-}
+define('APP_URL', 'http://mystreamviewer.com/');
 
 define('APP_TITLE', 'Stream Viewer Sample');
 
@@ -19,11 +15,11 @@ define('DB_NAME', 'db_name');
 define('DB_DATE_FORMAT', 'Y-m-d H:i:s');
 
 // GOOGLE
-define('GOOGLE_SERVER_KEY', 'AIzaSyCiyZrTTlc6xWtOMX3DcWgKJjbVRl1_E_o');
-define('GOOGLE_CLIENT_ID', '666918434391-n8o1qh503r6c1c854hnkfb47m1fej8gh.apps.googleusercontent.com');
-define('GOOGLE_CLIENT_SECRET', 'xpL93RNSPfGUjGnowXM_mL3C');
+define('GOOGLE_SERVER_KEY', 'myserverkey');
+define('GOOGLE_CLIENT_ID', 'myclientid.apps.googleusercontent.com');
+define('GOOGLE_CLIENT_SECRET', 'client_secret');
 define('GOOGLE_SIGNIN_SCOPE', 'profile email');
-define('GOOGLE_SITE_VERIFICATION', 'ruaRDD_lNeI396phJkcww2Y5NiSm5oWn9Spgc2pqAfE');
+define('GOOGLE_SITE_VERIFICATION', 'site_verification_key');
 
 // general
 define('DIR_VIEWS', 'views/');
